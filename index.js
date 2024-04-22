24ff1af700329ce2843a03e297bd4a4874a97bf1
wangjin <wjwcan10482@163.com>
2024-04-22 14:27:28 +0800

    update index.js

--- index.js
+++ index.js
@@ -2,3 +2,16 @@
 module.exports = function greet(name) {
    return `Hello, ${name}!`;
 };
+
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+readline.question('请输入你的名字：', name => {
+  // 使用我们定义的 greet 函数来生成问候语
+  const greeting = greet(name);
+  // 打印出问候语
+  console.log(greeting);
+  readline.close();
+});
